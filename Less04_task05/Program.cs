2fa08b5483ea2cfed821fd4971247cf49c38cbbb
Hayk <hayk.hayr3@gmail.com>
2020-12-06 23:45:41 +0400

    Add project files.

---
+++ Less04_task05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Less04_task05
+{
+	interface Interface
+	{
+		void Method();
+	}
+
+	abstract class AbstractClass : Interface
+	{
+		public abstract void Method();
+	}
+
+	class ConcreteClass : AbstractClass
+	{
+		public override void Method()
+		{
+			Console.WriteLine("Метод - реализация интерфейса в абстрактном классе.");
+		}
+	}
+
+	class Program
+	{
+		static void Main()
+		{
+			ConcreteClass instance = new ConcreteClass();
+			instance.Method();
+
+			Console.ReadKey();
+		}
+	}
+}
